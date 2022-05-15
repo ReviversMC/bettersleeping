91550956d574a6d81b37e02168edc915acb42d13
Julian Burner <burnerjulian@gmail.com>
2022-05-15 18:49:09 +0200

    Initialize EventListener on `onInitialize` event, not earlier

--- bettersleeping-1.17/src/main/java/com/github/reviversmc/bettersleeping/BetterSleeping117.java
+++ bettersleeping-1.17/src/main/java/com/github/reviversmc/bettersleeping/BetterSleeping117.java
@@ -6,10 +6,12 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class BetterSleeping117 implements ModInitializer {
-    public static EventHandler117 eventHandler = new EventHandler117();
+    public static EventHandler117 eventHandler;
 
     @Override
     public void onInitialize() {
+        eventHandler = new EventHandler117();
+
 		ServerTickEvents.END_SERVER_TICK.register(eventHandler::onTick);
     }
 }
--- bettersleeping-1.19/src/main/java/com/github/reviversmc/bettersleeping/BetterSleeping119.java
+++ bettersleeping-1.19/src/main/java/com/github/reviversmc/bettersleeping/BetterSleeping119.java
@@ -6,10 +6,12 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class BetterSleeping119 implements ModInitializer {
-    public static EventHandler119 eventHandler = new EventHandler119();
+    public static EventHandler119 eventHandler;
 
     @Override
     public void onInitialize() {
+        eventHandler = new EventHandler119();
+
 		ServerTickEvents.END_SERVER_TICK.register(eventHandler::onTick);
     }
 }
